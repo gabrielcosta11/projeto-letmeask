da2a52f1615b340e2b6c9c78570b6bb8a435414d
gabrielcosta11 <ganovo.costa@outlook.com>
2021-09-25 12:27:51 -0300

    first commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function Button() {
+    const [counter, setCounter] = useState(0)
+
+    function increment() {
+        setCounter(counter + 1)
+    }
+
+    return (
+        <button onClick={increment}>
+            {counter}
+        </button>
+    )
+}
+
+export {Button}
